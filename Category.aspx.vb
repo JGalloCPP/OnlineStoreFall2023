d765d588368b104c215590614b96b3878f5b1752
gallo <gallo@GALLO>
2023-11-15 15:35:57 -0800

    Add project files.

---
+++ Category.aspx.vb
@@ -0,0 +1,23 @@
+﻿Public Class Category
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Request.QueryString("SearchString") <> "" Then
+            SqlDSProductList.SelectCommand = "Select * From Product Where ProductName Like '%" & Request.QueryString("SearchString") & "%'"
+            SqlDSProductList.DataBind()
+            ' handle the lblProductList
+
+        Else
+            If Request.QueryString("MainCategoryID") <> "" Then
+                lblMainCategoryName.Text = Request.QueryString("MainCategoryName")
+                sqlDSSubCategory.SelectCommand = "Select * From Category Where Parent = " & CInt(Request.QueryString("MainCategoryID"))
+                If Request.QueryString("SubCategoryID") <> "" Then
+                    lblProductList.Text = "Product List for: " & Request.QueryString("SubCategoryName")
+                    SqlDSProductList.SelectCommand = "Select * From Product Where SubCategoryID = " & Int(Request.QueryString("SubCategoryID"))
+                    SqlDSProductList.DataBind()
+                End If
+            End If
+        End If
+    End Sub
+
+End Class
---
+++ ProductDetails.aspx.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Public Class ProductDetails
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Request.QueryString("ProductID") <> "" Then
+            Dim strConn As String = System.Configuration.ConfigurationManager.ConnectionStrings("ConnectionStringOnlineStore").ConnectionString
+            Dim connProduct As SqlConnection
+            Dim cmdProduct As SqlCommand
+            Dim drProduct As SqlDataReader
+            Dim strSQL As String = "Select * from Product Where ProductID = '" & Request.QueryString("ProductID") & "'"
+            connProduct = New SqlConnection(strConn)
+            cmdProduct = New SqlCommand(strSQL, connProduct)
+            connProduct.Open()
+            drProduct = cmdProduct.ExecuteReader(CommandBehavior.CloseConnection)
+            If drProduct.Read() Then
+                lblProductName.Text = drProduct.Item("ProductName")
+                lblProductNo.Text = drProduct.Item("ProductID")
+                lblProductPrice.Text = drProduct.Item("Price")
+            Else
+
+            End If
+        End If
+    End Sub
+
+End Class
